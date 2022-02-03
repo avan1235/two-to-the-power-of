b430bd18666ecaba6a09c1ca1d76abfc8db543e5
avan1235 <macieekprocyk@gmail.com>
2022-02-03 16:35:00 +0100

    migrate build scripts to kotlin

--- app/src/main/java/ml/dev/kotlin/poweroftwo/ui/Board.kt
+++ app/src/main/java/ml/dev/kotlin/poweroftwo/ui/Board.kt
@@ -7,13 +7,11 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import ml.dev.kotlin.poweroftwo.game.GameBoard
 import ml.dev.kotlin.poweroftwo.ui.theme.Background
 import ml.dev.kotlin.poweroftwo.ui.theme.Shapes
-import kotlin.math.min
 
 @Composable
 fun Board(gameBoard: GameBoard, blockSize: Dp) {
--- app/src/main/java/ml/dev/kotlin/poweroftwo/ui/Cell.kt
+++ app/src/main/java/ml/dev/kotlin/poweroftwo/ui/Cell.kt
@@ -14,7 +14,6 @@ import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import ml.dev.kotlin.poweroftwo.game.BoardCell
 import ml.dev.kotlin.poweroftwo.game.EmptyCell
 import ml.dev.kotlin.poweroftwo.game.NumberCell
